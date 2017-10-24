c105d070ec4c522f66a091bab9da4f06fef12378
cirops <cirops@gmail.com>
2017-10-23 22:03:47 -0200

    Fixing user feedback issues

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,19 @@
 $('#search_form').submit((event) => {
   const query = $('input[name="search"]').val();
   $.get('https://www.googleapis.com/books/v1/volumes?q='.concat(query), (data) => {
-    if (data.items.length <= 0) {
-      $('.search_n_results').text('No');
+    if (data.totalItems === 0) {
+      $('.search_query_p').html(`<strong>No</strong> results found for "<em>${query}</em>".`);
     } else {
+      $('.search_total_results').text(data.totalItems);
       $('.search_n_results').text(data.items.length);
+      $('.search_keyword').text(query);
+      const ul = $('<ul>');
+      for (let i = 0, l = data.items.length; i < l; i += 1) {
+        ul.append(`<li><strong>${data.items[i].volumeInfo.title}</strong> - ${data.items[i].volumeInfo.authors}`);
+      }
+      $('#results_box').append(ul);
     }
-    $('.search_keyword').text(query);
     $('.search_query_p').show();
-    const ul = $('<ul>');
-    for (let i = 0, l = data.items.length; i < l; i += 1) {
-      ul.append('<li><strong>'.concat(data.items[i].volumeInfo.title).concat('</strong> - ').concat(data.items[i].volumeInfo.authors));
-    }
-    $('#results_box').append(ul);
   });
   event.preventDefault();
 });
