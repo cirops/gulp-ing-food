1f08ee0e45b2444e2eb27b4c6369142c02843fc2
cirops <cirops@gmail.com>
2017-10-24 21:27:28 -0200

    fixing dirty search results

--- src/js/main.js
+++ src/js/main.js
@@ -4,20 +4,26 @@ $('#search_form').submit((event) => {
   $('.icon-container').show();
   const query = $('input[name="search"]').val();
   $.get('https://www.googleapis.com/books/v1/volumes?q='.concat(query), (data) => {
+    $('.results_data').hide();
+    $('.results_positive').hide();
     if (data.totalItems === 0) {
-      $('.search_query_p').html(`<strong>No</strong> results found for "<em>${query}</em>".`);
+      // $('.search_query_p').html(`<strong>No</strong> results found for "<em>${query}</em>".`);
+      $('.results_total').text('No');
+      $('.results_query').text(query);
     } else {
-      $('.search_total_results').text(data.totalItems);
-      $('.search_n_results').text(data.items.length);
-      $('.search_keyword').text(query);
+      $('.results_total').text(data.totalItems);
+      $('.results_page').text(data.items.length);
+      $('.results_query').text(query);
       const ul = $('<ul>');
       for (let i = 0, l = data.items.length; i < l; i += 1) {
         ul.append(`<li><strong>${data.items[i].volumeInfo.title}</strong> - ${data.items[i].volumeInfo.authors}`);
       }
+      $('.results_positive').show();
       $('#results_box').append(ul);
+      
     }
+    $('.results_data').show();
     $('.icon-container').hide();
-    $('.search_query_p').show();
   });
   event.preventDefault();
 });
