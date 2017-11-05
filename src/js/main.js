1379d15b6d72d70b6e791b132b12dfcd21221a2d
cirops <cirops@gmail.com>
2017-11-05 14:55:27 -0200

    refactor js

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,32 @@
 let searchPage = 1;
 let query = '';
 
-const $button = $('#btn_submit');
-const $buttonPrev = $('.search_prev');
-const $buttonNext = $('.search_next');
+const ui = {
+  submitButton: $('#btn_submit'),
+  previousButton: $('.search_prev'),
+  nextButton: $('.search_next'),
+  spinner: $('.icon-container'),
+  feedback: $('.results_data'),
+  form: $('#search_form'),
+  searchInput: $('#search_form input'),
+  resultContainer: $('#results_box'),
+};
+
+const showLoader = () => {
+  ui.resultContainer.html('');
+  ui.spinner.show();
+};
 
 function fetchPage(pageIndex) {
-  $('#results_box').html('');
-  $('.icon-container').show();
+  showLoader();
   searchPage = pageIndex;
 
   if (searchPage === 1) {
-    $buttonPrev.prop('disabled', true);
+    ui.previousButton.prop('disabled', true);
   } else {
-    $buttonPrev.prop('disabled', false);
+    ui.previousButton.prop('disabled', false);
   }
+
   $.ajax({
     url: `http://www.recipepuppy.com/api/?i=${query}&p=${searchPage}`,
     async: true,
@@ -23,31 +35,31 @@ function fetchPage(pageIndex) {
     jsonp: 'callback',
     jsonpCallback: 'fetchSuccess',
   }).catch(() => {
-    $('.results_data').text(`No more results for "${query}".`);
-    $('.icon-container').hide();
-    $('#btn_submit').attr('disabled', false);
-    $buttonNext.attr('disabled', true);
+    ui.feedback.text(`No more results for "${query}".`);
+    ui.spinner.hide();
+    ui.submitButton.attr('disabled', false);
+    ui.nextButton.attr('disabled', true);
   });
 }
 
-$('#search_form').submit((event) => {
-  $('#btn_submit').prop('disabled', true);
-  $('.results_data').hide();
-  $buttonPrev.hide();
-  $buttonNext.hide();
-  query = $('input[name="search"]').val();
+ui.form.submit((event) => {
+  ui.submitButton.prop('disabled', true);
+  ui.feedback.hide();
+  ui.previousButton.hide();
+  ui.nextButton.hide();
+  query = ui.searchInput.val();
 
   fetchPage(1);
   event.preventDefault();
 });
 
-$buttonPrev.click((event) => {
+ui.previousButton.click((event) => {
   fetchPage(searchPage - 1);
-  $buttonNext.attr('disabled', false);
+  ui.nextButton.attr('disabled', false);
   event.preventDefault();
 });
 
-$buttonNext.click((event) => {
+ui.nextButton.click((event) => {
   fetchPage(searchPage + 1);
   event.preventDefault();
 });
@@ -55,46 +67,48 @@ $buttonNext.click((event) => {
 window.fetchSuccess = (data) => {
   const recipes = data.results;
   if (recipes.length === 0) {
-    $('.results_data').text(`No results found for "${query}".`);
+    ui.feedback.text(`No results found for "${query}".`);
   } else {
-    $('.results_data').text(`Showing ${recipes.length} results for "${query}" on this page.`);
+    ui.feedback.text(`Showing ${recipes.length} results for "${query}" on this page.`);
     const ul = $('<ul>');
-    for (let i = 0, l = recipes.length; i < l; i += 1) {
-      ul.append(`
+    const recipesAsHtml = recipes.map(recipe => `
       <div class="card">
         <div class="row">
           <div class="col-md-auto thumb-container">
-              <a href="${recipes[i].href}" target="_blank"><img class="card-img-top" src="${recipes[i].thumbnail ? recipes[i].thumbnail: 'food-placeholder.png'}" alt="Card image cap"></a>
+              <a href="${recipe.href}" target="_blank"><img class="card-img-top" src="${recipe.thumbnail ? recipe.thumbnail : 'food-placeholder.png'}" alt="Card image cap"></a>
           </div>
           <div class="col-md-auto" style="max-width: 80vw">
             <div class="card-block">
-              <a href="${recipes[i].href}" target="_blank"><h4 class="card-title">${recipes[i].title}</h4></a>
-              <p class="card-text "><strong>Ingredients:</strong> ${recipes[i].ingredients}</p>
+              <a href="${recipe.href}" target="_blank"><h4 class="card-title">${recipe.title}</h4></a>
+              <p class="card-text "><strong>Ingredients:</strong> ${recipe.ingredients}</p>
             </div>
           </div>
         </div>
-      </div>`);
-    }
-    $('.search_prev').show();
-    $('.search_next').show();
-    $('#results_box').append(ul);
+      </div>
+    `);
+    ul.append(recipesAsHtml);
+    ui.previousButton.show();
+    ui.nextButton.show();
+    ui.resultContainer.append(ul);
   }
-  $('.results_data').show();
-  $('.icon-container').hide();
-  $('#btn_submit').attr('disabled', false);
+  ui.feedback.show();
+  ui.spinner.hide();
+  ui.submitButton.attr('disabled', false);
 };
 
 const checkInputValue = () => {
-  const value = $('#search_form input').val();
+  const value = ui.searchInput.val();
 
   if (value) {
-    $button.attr('disabled', false);
-    $button.css('cursor', 'pointer');
+    ui.submitButton.attr('disabled', false);
+    ui.submitButton.css('cursor', 'pointer');
   } else {
-    $button.attr('disabled', true);
-    $button.css('cursor', 'not-allowed');
+    ui.submitButton.attr('disabled', true);
+    ui.submitButton.css('cursor', 'not-allowed');
   }
 };
 
-$('#search_form input').on('keyup', checkInputValue);
-checkInputValue();
+$(() => {
+  ui.searchInput.on('keyup', checkInputValue);
+  checkInputValue();
+});
